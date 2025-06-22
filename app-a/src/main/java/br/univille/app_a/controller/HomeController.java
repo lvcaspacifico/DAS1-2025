7eba46274e2c68b269b3ad8170b8fdd96f437473
Lucas Pacífico <112137462+lvcaspacifico@users.noreply.github.com>
2025-06-22 20:01:56 +0000

    upd com atualizações da aula 18-06

--- app-a/src/main/java/br/univille/app_a/controller/HomeController.java
+++ app-a/src/main/java/br/univille/app_a/controller/HomeController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
 
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.HttpExtension;
+
+
 @RestController
 @RequestMapping("/api")
 public class HomeController {
@@ -13,6 +18,14 @@ public class HomeController {
 
     @GetMapping()
     public ResponseEntity index() {
+     
+        try {
+            DaprClient daprClient = new DaprClientBuilder().build();
+            daprClient.invokeMethod("app-b", "/api", null, HttpExtension.GET).block();
+        } catch (Exception e){
+            // TODO: Exception handling
+        }
+
         return ResponseEntity.ok().body("Hello from App A");
     }
 }
